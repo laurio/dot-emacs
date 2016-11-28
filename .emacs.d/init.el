cc9ec76beaa1cc10b0274ead7f1a8b7f5ba6d6c9
Lauri Oherd <lauri.oherd@gmail.com>
2016-11-28 06:14:47 +0200

    Add some customisations

---
+++ .emacs.d/init.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(calendar-week-start-day 1)
+ '(column-number-mode t)
+ '(cua-enable-cua-keys nil)
+ '(cua-mode t nil (cua-base))
+ '(display-time-day-and-date t)
+ '(epa-pinentry-mode (quote loopback))
+ '(ido-enable-flex-matching t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(ns-alternate-modifier (quote none))
+ '(ns-command-modifier (quote meta))
+ '(sql-postgres-options (quote ("-P" "pager=off" "-p 15433")))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
